a18d8510948ad4a63b9b8982b30a684e5357e013
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-08-07 20:08:38 +0300

    feat: enable opening product details on Product Details page

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchProductById } from "../../Redux/slices/productSlice";
+
 
 function ProductDetails() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { selectedProduct, loading } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProductById(id));
+  }, [dispatch, id]);
+
+  if (loading || !selectedProduct) return <p>Loading...</p>;
+
   return (
-    <div>ProductDetails</div>
-  )
+    <div className='p-8'>
+      <h2 className='text-2xl font-bold'>{selectedProduct.name}</h2>
+      <p className='mt-2'>{selectedProduct.description}</p>
+      <p className='mt-2 font-semibold'>Price : {selectedProduct.price} EGP</p>
+      <p className='mt-2'>weight: {selectedProduct.weight}</p>
+    </div>
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
+
