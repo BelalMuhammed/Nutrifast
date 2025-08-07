fde2ca0b6e84f8930fbb54545a521df5bad241de
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-08-07 17:24:51 +0300

    refactor the code to fix routing

--- src/Components/layout/LayOut.jsx
+++ src/Components/layout/LayOut.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import Footer from "../Footer/Footer";
 
-export default function LayOut() {
+export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Navbar />
-      <main className="flex-1 px-4 py-6 bg-gray-50">
+      <main className='flex-1 px-4 py-6 bg-gray-50'>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
