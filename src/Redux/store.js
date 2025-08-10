2424d5d9adb42987299090e6f12acfcba45eab91
Mariam Ahmed <mariam.a.abdelmaqsod98@gmail.com>
2025-08-10 19:25:47 +0300

    merge development into feature-register and resolve conflict

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,22 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productSlice";
-<<<<<<< HEAD
 import vendorReducer from "./slices/vendorSlice"
-=======
 import wishlistReducer from "./slices/wishListSlice";
 
 import cartReducer from "./slices/cartSlice";
->>>>>>> 9af3f38db76fdd2767eb94d33315522d70d39652
 export const store = configureStore({
   reducer: {
     products: productReducer,
     cart: cartReducer,
     auth: authReducer,
-<<<<<<< HEAD
     vendor: vendorReducer,
-=======
     wishlist: wishlistReducer,
->>>>>>> 9af3f38db76fdd2767eb94d33315522d70d39652
   },
 });
