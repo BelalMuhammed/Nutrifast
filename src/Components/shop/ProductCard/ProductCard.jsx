c2fa979003d06e7453158846eae4c77e2c793103
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-08-12 17:26:10 +0300

    justify the heart position

--- src/Components/shop/ProductCard/ProductCard.jsx
+++ src/Components/shop/ProductCard/ProductCard.jsx
@@ -11,52 +11,52 @@ function ProductCard({ product }) {
   const isInWishlist = wishlistItems.some((item) => item.id === product.id);
 
   return (
-    <div className="product bg-white rounded-2xl p-5 shadow-lg w-[280px]">
-      {/* Product Image */}
-      <Link to={`/product/${id}`}>
-        <img
-          src={image}
-          alt={name}
-          className="w-full object-contain mb-4 rounded-2xl"
-        />
-      </Link>
+    <div className='product bg-white rounded-2xl p-5 shadow-lg w-[280px] relative'>
+      {/* Wishlist Heart Button on top of image */}
+      <div className='relative h-[155px] rounded-2xl overflow-hidden mb-4'>
+        <Link to={`/product/${id}`}>
+          <img
+            src={image}
+            alt={name}
+            className='w-full  h-full object-cover mb-4 '
+          />
+        </Link>
+        <button
+          onClick={() => dispatch(toggleWishlistItem(product))}
+          className={`text-xl absolute top-2 right-2 ${
+            isInWishlist ? "text-red-500" : "text-gray-600"
+          }`}>
+          <FaHeart />
+        </button>
+      </div>
 
       {/* Name */}
-      <h3 className="text-lg font-semibold text-app-secondary mb-2">{name}</h3>
+      <h3 className='text-lg font-semibold text-app-secondary mb-2'>{name}</h3>
 
       {/* Tags */}
-      <div className="flex flex-wrap gap-2 mb-3">
+      <div className='flex flex-wrap gap-2 mb-3'>
         {tags.map((tag) => (
           <span
             key={tag}
-            className="px-2 py-1 text-xs rounded-full bg-app-quaternary text-app-secondary font-medium"
-          >
+            className='px-2 py-1 text-xs rounded-full bg-app-quaternary text-app-secondary font-medium'>
             {tag}
           </span>
         ))}
       </div>
 
       {/* Short Description */}
-      <p className="text-sm text-app-secondary mb-4 line-clamp-2">
+      <p className='text-sm text-app-secondary mb-4 line-clamp-2'>
         {description}
       </p>
 
       {/* Price */}
-      <span className="text-lg font-bold text-app-secondary mb-4 block">
+      <span className='text-lg font-bold text-app-secondary mb-4 block'>
         {price} EGP
       </span>
 
       {/* Add to Cart Button */}
-      <div className="flex justify-between ">
+      <div className='flex justify-between '>
         <AddButton product={product} />
-        <button
-          onClick={() => dispatch(toggleWishlistItem(product))}
-          className={`text-xl ${
-            isInWishlist ? "text-red-500" : "text-gray-600"
-          }`}
-        >
-          <FaHeart />
-        </button>
       </div>
     </div>
   );
