1b6c3ca54c312e4626dfe6fd92b92ffc9a47d5d3
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-08-13 01:20:35 +0300

    refactor SortSelect component layout and add sorting logic utility

--- src/Components/shop/SortSelect/SortSelect.jsx
+++ src/Components/shop/SortSelect/SortSelect.jsx
@@ -2,14 +2,15 @@ import { Select } from "flowbite-react";
 
 function SortSelect({ sortOption, setSortOption }) {
   return (
-    <div className='flex items-center gap-3 mb-6'>
-      <span className='text-sm font-semibold text-gray-800'>Sort By</span>
+    <div className='flex items-center gap-4 mb-6  px-5 py-3 '>
+      <span className='text-base font-semibold text-app-primary mr-2'>
+        Sort By
+      </span>
       <Select
         value={sortOption}
         onChange={(e) => setSortOption(e.target.value)}
-        className='w-64'
-      >
-        <option value=''>--Sort by--</option>
+        className='w-56 rounded-lg border border-gray-200 focus:ring-green-500 focus:border-green-500 text-gray-700 font-medium bg-gray-50 '>
+        <option value=''>-- Sort by --</option>
         <option value='price-high'>Price: High to Low</option>
         <option value='price-low'>Price: Low to High</option>
         <option value='rating-high'>Top Rated</option>
