2a9acb66836e2ebe47151380d8ad58a242f4e2c3
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-08-23 22:47:49 +0300

    remove ThemeInit component from main render

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeInit />
     <Provider store={store}>
       {" "}
       <App />
