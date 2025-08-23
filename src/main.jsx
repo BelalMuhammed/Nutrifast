2bf266ec0376d1529d9c65c17a7e4159584ad1eb
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-08-23 22:43:56 +0300

    add ThemeInit component to main render

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ThemeInit />
     <Provider store={store}>
       {" "}
       <App />
