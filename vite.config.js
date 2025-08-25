8004946e07ce4a71bad4cd2ab0ee37dc0638f487
BelalMuhammed <Belal.Muhammad950@gmail.com>
2025-08-25 06:39:24 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
-import path from 'node:path'
+import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-   resolve: {
+  base: process.env.VITE_BASE_PATH || "/Nutrifast",
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
